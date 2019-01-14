a7816169752f334f4959078a224d13c7cad57b42
Koki Ando <basket.koki.bowl@gmail.com>
2019-01-14 20:40:18 +0900

    New function

---
+++ R/getPopulationStats.R
@@ -0,0 +1,34 @@
+#'
+#' @param api_key
+#' @param cityCode
+#' @param prefCode
+#' 
+#' @author Koki Ando
+#' 
+#' @import RCurl
+#' @import jsonlite
+#' @import dplyr
+#'
+#' @examples
+#' \dontrun{
+#'   kyoto <- getPopulationStats(api_key, cityCode=26100, prefCode=26)
+#' }
+#' 
+#' @export
+
+getPopulationStats <- function(api_key, cityCode, prefCode){
+  base_url<-'https://opendata.resas-portal.go.jp/'
+  api<-'api/v1/population/composition/perYear?cityCode='
+  getdata.json<-RCurl::getURL(paste0(base_url, api, cityCode, '&prefCode=', prefCode), 
+                              httpheader = paste('X-API-KEY:', api_key))
+  data <- fromJSON(getdata.json)$result$data
+  df <- do.call(cbind, data$data) %>% 
+    data.frame()
+  names(df)[1] <- "年"
+  df <- df %>% 
+    select(-contains("year"))
+  names(df) <- c("年", "総人口", "年少人口", "年少人口比率", "生産年齢人口", "生産年齢人口比率", "老年人口", "老年人口比率")
+  df %>% 
+    mutate(cityCode = cityCode, prefCode = prefCode)
+}
+
